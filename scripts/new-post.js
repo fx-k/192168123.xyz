e26bf9b093db8e9431df88ab6b185408f07a4779
fx-k <admin@fxit.top>
2024-07-14 00:32:23 +0800

    update file creation logic in new-post.js.

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -2,12 +2,27 @@ const fs = require('fs').promises
 const path = require('path')
 const dayjs = require('dayjs')
 
-// npm run new:post filename title [tag1] [tag2] ...
+// npm run new:post title [tag1] [tag2] ...
 const createPost = async () => {
-  const [, , filename, title, ...tags] = process.argv
+  const [, , title, ...tags] = process.argv
+  const dateStr = dayjs().format('YYYY-MM-DD')
+  const postsDir = path.join(process.cwd(), 'posts')
+
+  let filename
+  let index = 1
+
+  while (true) {
+    filename = `${dateStr}-new-${index}.mdx`
+    try {
+      await fs.access(path.join(postsDir, filename))
+      index++
+    } catch {
+      break
+    }
+  }
 
   await fs.writeFile(
-    path.join(process.cwd(), `posts/${dayjs().format('YYYY-MM-DD')}-${filename}.mdx`),
+    path.join(postsDir, filename),
     `---
 title: '${title}'
 date: '${new Date().toISOString()}'
