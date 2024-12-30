5781b44aee6d6ec8b73844c996c419cfb531c5ad
fx-k <admin@fxit.top>
2024-12-30 13:04:06 +0800

    Move some variables into config.

--- site.config.js
+++ site.config.js
@@ -32,6 +32,33 @@ module.exports = {
     { name: '久伴博客', link: 'https://jiub.ren/' },
     { name: 'Sonic853', link: 'http://blog.853lab.com/' },
   ],
+  projects: [
+    {
+      name: '192168123.xyz',
+      desc: "小可の聚集地（本站已开源，帮我点点Star叭 orz）",
+      url: 'https://github.com/fx-k/192168123.xyz',
+    },
+    {
+      name: '繁星MC服务器',
+      desc: "Fancy World - 一个创立于2014年的MCPE(BE)公益服务器!",
+      url: 'https://mc.fxit.top',
+    },
+    {
+      name: 'FXCloud - 私有云',
+      desc: "233456.xyz, 一个对外分享的私有云平台。",
+      url: 'https://233456.xyz',
+    },
+    {
+      name: 'Server Status - 探针',
+      desc: "古人云，有朋自远方来，以针会友～",
+      url: 'https://dash.status.192168123.xyz',
+    },
+    {
+      name: '停止跟踪 - 净化分享链接',
+      desc: "一键去除小红书/哔哩哔哩链接中的跟踪信息，并生成短链。",
+      url: 'https://fwd.pp.ua/clean',
+    },
+  ],
   // en | zh-CN
   language: 'zh-CN',
   // 侧边目录
--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -2,29 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 import DesktopOnly from '@/components/DesktopOnly'
 import { ArrowRight } from '@/components/icons'
+import config from 'config'
 
-const projects = [
-  {
-    name: '192168123.xyz',
-    desc: "小可の聚集地（本站已开源，帮我点点Star叭 orz）",
-    url: 'https://github.com/fx-k/192168123.xyz',
-  },
-  {
-    name: '繁星MC服务器',
-    desc: "Fancy World - 一个创立于2014年的MCPE(BE)公益服务器!",
-    url: 'https://mc.fxit.top',
-  },
-  {
-    name: 'FXCloud - 私有云',
-    desc: "233456.xyz, 一个对外分享的私有云平台。",
-    url: 'https://233456.xyz',
-  },
-  {
-    name: 'Server Status - 探针',
-    desc: "古人云，有朋自远方来，以针会友～",
-    url: 'https://dash.status.192168123.xyz',
-  },
-]
+const projects = config.projects;
 
 const Projects = () => {
   return (
