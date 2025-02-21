b0710b4ccea1a1eaec641de504c8d820f08e4d40
fx-k <admin@fxit.top>
2025-02-21 22:24:34 +0800

    add friends link.

--- site.config.js
+++ site.config.js
@@ -28,6 +28,7 @@ module.exports = {
     { name: 'HanCanonのBlog', link: 'https://blog.hancanon.com/' },
     { name: 'Diaoan\'s Blog', link: 'https://diaoan.xyz/' },
     { name: 'UsubeniFantasy', link: 'https://ssshooter.com/' },
+    { name: 'Sukka\'s Blog', link: 'https://blog.skk.moe/' },
   ],
   friends_invalid: [
     { name: '遇见心流', link: 'http://yujianxinliu.com/' },
